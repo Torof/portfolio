d779dbc0a682f6557b0cb3d3086439cf3dd74995
Torof <scottpanam@protonmail.com>
2024-06-15 10:03:08 +0200

    feat(core): implement ERC-6551 binding in TownNFT

--- town_builder/contracts/src/core/TownNFT.sol
+++ town_builder/contracts/src/core/TownNFT.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "erc6551/interfaces/IERC6551Registry.sol";
 
 /**
  * @title TownNFT
@@ -18,24 +19,74 @@ contract TownNFT is ERC721Enumerable, Ownable {
     
     // Events
     event TownCreated(uint256 indexed tokenId, address owner);
+    event TownAccountCreated(uint256 indexed tokenId, address account);
+    
+    // ERC-6551 Registry address (canonical registry)
+    address public immutable erc6551Registry;
+    
+    // Town Account implementation address
+    address public immutable townAccountImplementation;
+    
+    // Salt used for account creation
+    bytes32 public immutable accountSalt;
     
     /**
      * @dev Constructor
+     * @param _registry Address of the ERC-6551 Registry
+     * @param _implementation Address of the TownAccountGame implementation
      */
-    constructor() ERC721("Town Builder", "TOWN") Ownable(msg.sender) {}
+    constructor(
+        address _registry,
+        address _implementation
+    ) ERC721("Town Builder", "TOWN") Ownable(msg.sender) {
+        require(_registry != address(0), "Invalid registry address");
+        require(_implementation != address(0), "Invalid implementation address");
+        
+        erc6551Registry = _registry;
+        townAccountImplementation = _implementation;
+        accountSalt = bytes32(0); // Using 0 as salt, could use other values
+    }
     
     /**
      * @dev Creates a new town for the caller
      * @return tokenId The ID of the newly created town
      */
     function createTown() external returns (uint256) {
-        require(balanceOf(msg.sender) == 0, AlreadyOwnsTown());
+        require(balanceOf(msg.sender) == 0, "AlreadyOwnsTown()");
         
         uint256 tokenId = totalSupply() + 1;
         _safeMint(msg.sender, tokenId);
         
+        // Create token bound account for this NFT
+        address account = IERC6551Registry(erc6551Registry).createAccount(
+            townAccountImplementation,
+            accountSalt,
+            block.chainid,
+            address(this),
+            tokenId
+        );
+        
         emit TownCreated(tokenId, msg.sender);
+        emit TownAccountCreated(tokenId, account);
+        
         return tokenId;
+    }
+    
+    /**
+     * @dev Get the token bound account address for a token
+     * @param tokenId The ID of the token
+     * @return The address of the token bound account
+     */
+    function getTownAccount(uint256 tokenId) external view returns (address) {
+        require(_exists(tokenId), "TownDoesNotExist()");
+        
+        return IERC6551Registry(erc6551Registry).account(
+            townAccountImplementation,
+            accountSalt,
+            block.chainid,
+            address(this),
+            tokenId
+        );
     }
     
     /**
@@ -44,7 +95,7 @@ contract TownNFT is ERC721Enumerable, Ownable {
     function _update(address to, uint256 tokenId, address auth) internal virtual override returns (address) {
         // Prevent transfers to addresses that already have towns
         if (to != address(0)) {
-            require(balanceOf(to) == 0, RecipientAlreadyOwnsTown());
+            require(balanceOf(to) == 0, "RecipientAlreadyOwnsTown()");
         }
         
         return super._update(to, tokenId, auth);
